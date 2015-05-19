3b6085d1ead7f8fb30fbe5e54f163e59eb60ad3f
Sagyouin <the_worker@Takeshi-no-Mac-mini.local>
2015-05-20 00:59:33 +0900

    update

--- www/src/stage/main.js
+++ www/src/stage/main.js
@@ -1,12 +1,25 @@
 // You can use either `new PIXI.WebGLRenderer`, `new PIXI.CanvasRenderer`, or `PIXI.autoDetectRenderer`
 // which will try to choose the best renderer for the environment you are in.
-var renderer = new PIXI.WebGLRenderer(800, 600);
+var width    = screen.availWidth;
+var height   = screen.availHeight;
+var renderer = new PIXI.WebGLRenderer(width, height);
+
+// The renderer will create a canvas element for you that you can then insert into the DOM.
+document.body.appendChild(renderer.view);
 
 // You need to create a root container that will hold the scene you want to draw.
-var stage = new PIXI.Container();
+var stage = new PIXI.Container(0x000000);
 
 // kick off the animation loop (defined below)
 animate();
+
+
+var word = "Hello World!";
+var style = {font:'bold 60pt Arial', fill:'white'}; // 文字サイズや色など
+var textobj = new PIXI.Text(word, style); // テキストオブジェクトの生成
+textobj.position.x = 0;  // 表示位置(x)
+textobj.position.y = 0; // 表示位置(y)
+stage.addChild(textobj);
 
 function animate() {
     // start the timer for the next animation loop
